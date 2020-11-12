132b64fc74286483059cf238f2a5e4dcf9ec4c68
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2020-11-12 21:02:45 +0300

    move hls.nix to local

--- nixpkgs/mkHlsPkgs.nix
+++ nixpkgs/mkHlsPkgs.nix
@@ -1,5 +1,5 @@
 { ghcVersion, sources }: let
-  hlsPkgs = import sources.nix-hls {
+  hlsPkgs = import ./hls.nix {
     inherit sources;
     unstable = true;
     config = {
@@ -9,6 +9,15 @@
       haskell-nix.hackage.index = {
         state = "2020-11-11T00:00:00Z";
         sha256 = "c8f87a190b33cfadbc08ddcd687d224845155e41b23600dddd94a8716b408be5";
+      };
+      # An alternative to adding `--sha256` comments into the
+      # cabal.project file:
+      #   sha256map =
+      #     { "https://github.com/jgm/pandoc-citeproc"."0.17"
+      #         = "0dxx8cp2xndpw3jwiawch2dkrkp15mil7pyx7dvd810pwc22pm2q"; };
+      haskell-nix.hackage.sha256map = {
+        "https://github.com/bubba/brittany"."c59655f10d5ad295c2481537fc8abf0a297d9d1c" = "1rkk09f8750qykrmkqfqbh44dbx1p8aq1caznxxlw8zqfvx39cxl";
+        "https://github.com/bubba/hie-bios"."cec139a1c3da1632d9a59271acc70156413017e7" = "1iqk55jga4naghmh8zak9q7ssxawk820vw8932dhympb767dfkha";
       };
       haskell-nix.nixpkgs-pin = "nixpkgs-2009";
       haskell-nix.plan = {};
