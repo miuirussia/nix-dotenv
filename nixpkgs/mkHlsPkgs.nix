b5bfc05cc51cd0a3b47e4234cffff441a39728c4
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2020-11-12 22:00:08 +0300

    fix url

--- nixpkgs/mkHlsPkgs.nix
+++ nixpkgs/mkHlsPkgs.nix
@@ -16,8 +16,8 @@
       #     { "https://github.com/jgm/pandoc-citeproc"."0.17"
       #         = "0dxx8cp2xndpw3jwiawch2dkrkp15mil7pyx7dvd810pwc22pm2q"; };
       haskell-nix.hackage.sha256map = {
-        "https://github.com/bubba/brittany"."c59655f10d5ad295c2481537fc8abf0a297d9d1c" = "1rkk09f8750qykrmkqfqbh44dbx1p8aq1caznxxlw8zqfvx39cxl";
-        "https://github.com/bubba/hie-bios"."cec139a1c3da1632d9a59271acc70156413017e7" = "1iqk55jga4naghmh8zak9q7ssxawk820vw8932dhympb767dfkha";
+        "https://github.com/bubba/brittany.git"."c59655f10d5ad295c2481537fc8abf0a297d9d1c" = "1rkk09f8750qykrmkqfqbh44dbx1p8aq1caznxxlw8zqfvx39cxl";
+        "https://github.com/bubba/hie-bios.git"."cec139a1c3da1632d9a59271acc70156413017e7" = "1iqk55jga4naghmh8zak9q7ssxawk820vw8932dhympb767dfkha";
       };
       haskell-nix.nixpkgs-pin = "nixpkgs-2009";
       haskell-nix.plan = {};
