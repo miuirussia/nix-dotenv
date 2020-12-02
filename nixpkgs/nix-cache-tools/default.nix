d4459ff01d75ddd4c4ab7bd0e023c0fa7f120e7f
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2020-12-02 21:02:23 +0300

    More verbose

--- nixpkgs/nix-cache-tools/default.nix
+++ nixpkgs/nix-cache-tools/default.nix
@@ -20,10 +20,10 @@ let
     STORE_URL=''${NIX_UPLOAD_STORE_URL:?Please, define binary cache url. Example: s3://nix-cache?profile=default&endpoint=s3.server.m}
 
     echo "Signing store paths..."
-    nix sign-paths --recursive --key-file ''${STORE_PRIVATE_KEY} $@
+    nix sign-paths -v --recursive --key-file ''${STORE_PRIVATE_KEY} $@
     rm $STORE_PRIVATE_KEY
     echo "Uploading store paths..."
-    nix copy --to ''${STORE_URL} $@
+    nix copy -v --to ''${STORE_URL} $@
     echo "...done!"
   '';
 in
