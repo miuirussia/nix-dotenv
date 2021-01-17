19e111b57d17b2730b8720202a2c458d78ee221b
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2021-01-17 14:56:55 +0300

    Add apply-refact

--- nixpkgs/mkHlsPkgs.nix
+++ nixpkgs/mkHlsPkgs.nix
@@ -16,6 +16,7 @@
       #     { "https://github.com/jgm/pandoc-citeproc"."0.17"
       #         = "0dxx8cp2xndpw3jwiawch2dkrkp15mil7pyx7dvd810pwc22pm2q"; };
       haskell-nix.hackage.sha256map = {
+        "https://github.com/mpickering/apply-refact.git"."4fbd3a3a9b408bd31080848feb6b78e13c3eeb6d" = "106jnfr0mbv1p0xfpy19sd2bk222f4737pb291rqllzpcqii8h60";
         "https://github.com/bubba/brittany.git"."c59655f10d5ad295c2481537fc8abf0a297d9d1c" = "1rkk09f8750qykrmkqfqbh44dbx1p8aq1caznxxlw8zqfvx39cxl";
         "https://github.com/bubba/hie-bios.git"."cec139a1c3da1632d9a59271acc70156413017e7" = "1iqk55jga4naghmh8zak9q7ssxawk820vw8932dhympb767dfkha";
       };
