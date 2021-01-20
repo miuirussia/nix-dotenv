1221c114d95e390688cec302f4526e10e29fc420
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2021-01-21 00:14:56 +0300

    bump deps

--- nixpkgs/mkHlsPkgs.nix
+++ nixpkgs/mkHlsPkgs.nix
@@ -7,8 +7,8 @@
       haskell-nix.useMaterialization = false;
       haskell-nix.checkMaterialization = false;
       haskell-nix.hackage.index = {
-        state = "2020-12-15T00:00:00Z";
-        sha256 = "0c93ab89049db197ae43d146eb84ccd2972cd0dfd57ecaa4721bb76a38f0cb32";
+        state = "2021-01-20T00:00:00Z";
+        sha256 = "8622e19e5e5b21c714db6cb63147da9984c558568ca1b3ab4dc8b8a30d660fb3";
       };
       # An alternative to adding `--sha256` comments into the
       # cabal.project file:
@@ -17,6 +17,7 @@
       #         = "0dxx8cp2xndpw3jwiawch2dkrkp15mil7pyx7dvd810pwc22pm2q"; };
       haskell-nix.hackage.sha256map = {
         "https://github.com/mpickering/apply-refact.git"."4fbd3a3a9b408bd31080848feb6b78e13c3eeb6d" = "106jnfr0mbv1p0xfpy19sd2bk222f4737pb291rqllzpcqii8h60";
+        "https://github.com/alanz/ghc-exactprint.git"."6748e24da18a6cea985d20cc3e1e7920cb743795" = "18r41290xnlizgdwkvz16s7v8k2znc7h215sb1snw6ga8lbv60rb";
         "https://github.com/bubba/brittany.git"."c59655f10d5ad295c2481537fc8abf0a297d9d1c" = "1rkk09f8750qykrmkqfqbh44dbx1p8aq1caznxxlw8zqfvx39cxl";
         "https://github.com/bubba/hie-bios.git"."cec139a1c3da1632d9a59271acc70156413017e7" = "1iqk55jga4naghmh8zak9q7ssxawk820vw8932dhympb767dfkha";
       };
