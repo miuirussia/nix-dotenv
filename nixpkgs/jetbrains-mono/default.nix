fc36476c44d8b108e386c0ae94732dc7e887b615
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2021-03-31 19:04:12 +0300

    Reorganize files

--- nixpkgs/jetbrains-mono/default.nix
+++ nixpkgs/jetbrains-mono/default.nix
@@ -1,55 +1,19 @@
-{ pkgs, lib, sources, ... }:
+{ pkgs, lib, sources, font-patcher, ... }:
 
 let
   version = "master";
-  fetchGitHubFiles = pkgs.callPackage ./build-support/fetchgithubfiles { };
-  font-patcher-repo = {
-    owner = "ryanoasis";
-    repo = "nerd-fonts";
-    rev = "ac5a5f0b591170dbecfcd503aa22a564cd88be1d";
-  };
-  font-patcher-src = fetchGitHubFiles {
-    name = "nerdfonts-patcher-${font-patcher-repo.rev}-src";
-    inherit (font-patcher-repo) owner repo rev;
-    paths = [
-      "font-patcher"
-      "src/glyphs"
-    ];
-    sha256 = "1yf12p4dsfy5pqnmn28q7pbadr63dpymmx8xzck21sbwl5qhlfzm";
-  };
-  font-patcher = pkgs.stdenvNoCC.mkDerivation {
-    name = "nerdfonts-patcher-${font-patcher-repo.rev}";
-
-    src = font-patcher-src;
-
-    phases = [ "unpackPhase" "buildPhase" "installPhase" "fixupPhase" ];
-    propagatedBuildInputs = [
-      (pkgs.python3.withPackages (ps: with ps; [ fontforge fonttools configparser ]))
-      pkgs.ttfautohint
-    ];
-
-    buildPhase = ''
-      substituteInPlace font-patcher \
-        --replace '__dir__ + "/src/glyphs/"' "\"$out/share/nerdfonts/glyphs/\""
-    '';
-
-    installPhase = ''
-      install -m 755 -D font-patcher $out/bin/nerdfonts-patcher
-      install -m 644 -D src/glyphs/* -t $out/share/nerdfonts/glyphs/
-    '';
-  };
 in
 pkgs.stdenv.mkDerivation {
   name = "JetBrainsMono-${version}";
 
   src = sources.JetBrainsMono;
 
-  buildInputs = [pkgs.fontforge font-patcher];
+  buildInputs = with pkgs; [fontforge font-patcher];
 
   buildPhase = ''
     export PYTHONIOENCODING=utf8
     for filename in ./fonts/ttf/*.ttf; do
-      fontforge -script ${font-patcher}/bin/nerdfonts-patcher $filename -c -out ./out
+      fontforge -script ${font-patcher}/bin/font-patcher $filename -c -out ./out
     done
   '';
 
--- nixpkgs/package.nix
+++ nixpkgs/package.nix
@@ -31,6 +31,45 @@ let
   hlsPkgs884 = mkHlsPkgs { ghcVersion = "ghc884"; inherit sources; };
   hlsPkgs8103 = mkHlsPkgs { ghcVersion = "ghc8103"; inherit sources; };
   hlsPkgs8104 = mkHlsPkgs { ghcVersion = "ghc8104"; inherit sources; };
+  fetchGitHubFiles = iohkPkgs.callPackage ./build-support/fetchgithubfiles { };
+
+  font-patcher-repo = {
+    owner = "ryanoasis";
+    repo = "nerd-fonts";
+    rev = "ac5a5f0b591170dbecfcd503aa22a564cd88be1d";
+  };
+
+  font-patcher-src = fetchGitHubFiles {
+    name = "font-patcher-${font-patcher-repo.rev}-src";
+    inherit (font-patcher-repo) owner repo rev;
+    paths = [
+      "font-patcher"
+      "src/glyphs"
+    ];
+    sha256 = "1yf12p4dsfy5pqnmn28q7pbadr63dpymmx8xzck21sbwl5qhlfzm";
+  };
+
+  font-patcher = iohkPkgs.stdenvNoCC.mkDerivation {
+    name = "font-patcher-${font-patcher-repo.rev}";
+
+    src = font-patcher-src;
+
+    phases = [ "unpackPhase" "buildPhase" "installPhase" "fixupPhase" ];
+    propagatedBuildInputs = with iohkPkgs; [
+      (python3.withPackages (ps: with ps; [ fontforge fonttools configparser ]))
+      ttfautohint
+    ];
+
+    buildPhase = ''
+      substituteInPlace font-patcher \
+        --replace '__dir__ + "/src/glyphs/"' "\"$out/share/nerdfonts/glyphs/\""
+    '';
+
+    installPhase = ''
+      install -m 755 -D font-patcher $out/bin/font-patcher
+      install -m 644 -D src/glyphs/* -t $out/share/nerdfonts/glyphs/
+    '';
+  };
 in
 {
   # allowBroken = true;
@@ -55,7 +94,7 @@ in
           src = sources.reattach-to-user-namespace;
         }
       );
-      jetbrains-mono = pkgs.callPackage ./jetbrains-mono/default.nix { inherit sources; };
+      jetbrains-mono = pkgs.callPackage ./jetbrains-mono/default.nix { inherit sources; inherit font-patcher; };
 
       kitty = pkgs.kitty.overrideAttrs (
         prev: {
@@ -146,5 +185,6 @@ in
       neovimUtils = pkgs.neovimUtils.override { nodejs = pkgs.nodejs-14_x; };
 
       inherit nodejs-14_x;
+      inherit font-patcher;
     };
 }
