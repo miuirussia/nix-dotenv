9720bd2378bc248ed6b0a2a4a18d2d53c4ab1a56
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2021-04-13 22:23:37 +0300

    vscode

--- nixpkgs/vscode/package/default.nix
+++ nixpkgs/vscode/package/default.nix
@@ -1,12 +1,10 @@
-{ fetchurl, vscodium }:
+{ fetchurl, vscode }:
 
-vscodium.overrideAttrs (_: {
-  version = builtins.readFile ./version;
-  src = fetchurl (builtins.fromJSON (builtins.readFile ./source.json));
-  installPhase = ''
-    mkdir -p "$out/Applications/VSCodium.app" $out/bin
-    cp -r ./* "$out/Applications/VSCodium.app"
-    ln -s "$out/Applications/VSCodium.app/Contents/Resources/app/bin/codium" $out/bin/codium
-    ln -s "$out/Applications/VSCodium.app/Contents/Resources/app/bin/codium" $out/bin/code
-  '';
-})
+vscode.overrideAttrs (
+  _: {
+    pname = "vscode-latest";
+    version = builtins.readFile ./version;
+
+    src = fetchurl (builtins.fromJSON (builtins.readFile ./source.json));
+  }
+)
--- nixpkgs/vscode/package/update.sh
+++ nixpkgs/vscode/package/update.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 
 cd "$(pwd)/$(dirname ${BASH_SOURCE})"
-version=$(curl "https://api.github.com/repos/VSCodium/vscodium/tags" | jq -r ".[0].name")
-if [ -z "$version" ]
-then
-  echo "version fetch failed"
-  exit 1
-fi
-
-url="https://github.com/VSCodium/vscodium/releases/download/$version/VSCodium-darwin-x64-$version.zip"
-sha256=$(nix-prefetch-url $url)
-
-echo -n $version | tee version
-echo "{}" \
-  | jq ".url=\"$url\" | .sha256=\"$sha256\" | .name=\"VSCodium.zip\"" \
-  | tee source.json
+url="https://update.code.visualstudio.com/latest/darwin/stable"
+new_url="$(curl -LsI -o /dev/null -w %{url_effective} $url)"
+echo "{}" | jq --arg url  "$new_url" \
+               --arg sha256 "$(nix-prefetch-url $url)" \
+               '.url = $url | .sha256 = $sha256' > source.json
+echo "$new_url" | grep -Po '\/([a-z0-9]{40})\/' | tr -d '\n\/' > version
