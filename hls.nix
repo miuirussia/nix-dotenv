5c52d264d722a942c0e267c15f73051b0e1248f4
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2021-05-28 22:25:19 +0300

    Optimize hls build

--- hls.nix
+++ hls.nix
@@ -1,13 +1,6 @@
-{ version ? "865" }:
-
 let
   sources = import ./sources;
   nixpkgsConfig = import ./nixpkgs/config.nix;
   pkgs = import sources.nixpkgs-unstable { config = nixpkgsConfig; };
 in
-pkgs.buildEnv {
-  name = "hls-" + version;
-  paths = [
-    pkgs."hls-ghc${version}"
-  ];
-}
+pkgs.hls
--- nixpkgs/package.nix
+++ nixpkgs/package.nix
@@ -2,35 +2,7 @@
 
 let
   haskellNix = import (sources."haskell.nix") {};
-  iohkArgs = haskellNix.nixpkgsArgs // {
-    overlays = haskellNix.nixpkgsArgs.overlays ++ [
-      (
-        self: super: {
-          haskell-nix = super.haskell-nix // {
-            compiler = super.haskell-nix.compiler // {
-              ghc844 = super.haskell-nix.compiler.ghc844.overrideAttrs (
-                prev: {
-                  src = prev.src.overrideAttrs (
-                    prevSrc: {
-                      patches = prevSrc.patches ++ [
-                        ./ghc/fix-ghc844.diff
-                      ];
-                    }
-                  );
-                }
-              );
-            };
-          };
-        }
-      )
-    ];
-  };
-  iohkPkgs = import sources.nixpkgs-unstable iohkArgs;
-  mkHlsPkgs = import ./mkHlsPkgs.nix;
-  hlsPkgs865 = mkHlsPkgs { ghcVersion = "ghc865"; inherit sources; };
-  hlsPkgs884 = mkHlsPkgs { ghcVersion = "ghc884"; inherit sources; };
-  hlsPkgs8103 = mkHlsPkgs { ghcVersion = "ghc8103"; inherit sources; };
-  hlsPkgs8104 = mkHlsPkgs { ghcVersion = "ghc8104"; inherit sources; };
+  iohkPkgs = import sources.nixpkgs-unstable haskellNix.nixpkgsArgs;
   fetchGitHubFiles = iohkPkgs.callPackage ./build-support/fetchgithubfiles { };
 
   font-patcher-repo = {
@@ -108,11 +80,27 @@ in
 
       haskell-nix = iohkPkgs.haskell-nix;
 
-      hls-ghc865 = hlsPkgs865.server;
-      hls-ghc884 = hlsPkgs884.server;
-      hls-ghc8103 = hlsPkgs8103.server;
-      hls-ghc8104 = hlsPkgs8104.server;
-      hls-wrapper = hlsPkgs8104.wrapper;
+      hls = let
+        mkHlsPackage = { ghcVersion }: import sources.nix-haskell-hls {
+          nixpkgs-pin = "nixpkgs-unstable";
+          inherit ghcVersion;
+          hlsUnstable = true;
+        };
+
+        hls865 = mkHlsPackage { ghcVersion = "ghc865"; };
+        hls884 = mkHlsPackage { ghcVersion = "ghc884"; };
+        hls8104 = mkHlsPackage { ghcVersion = "ghc8104"; };
+      in pkgs.buildEnv {
+        name = "haskell-language-server";
+
+        paths = [
+          hls865.hls-renamed
+          hls884.hls-renamed
+          hls8104.hls-renamed
+          hls8104.hls-wrapper
+          hls8104.hls-wrapper-nix
+        ];
+      };
 
       haskell = pkgs.haskell // {
         compiler = pkgs.haskell.compiler // {
